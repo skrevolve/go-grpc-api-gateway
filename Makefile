54596b562a82bb0b6cc532113d839102595d212e
skrevolve <sukyu0919@naver.com>
2023-05-15 16:44:00 +0900

    init

--- Makefile
+++ Makefile
@@ -5,8 +5,10 @@ run: build
 	./bin/micro
 
 proto:
-	protoc --go_out=. --go_opt=paths=source_relative \
-	--go-grpc_out=. --go-grpc_opt=paths=source_relative \
-	proto/user/user.proto
+	protoc -I ./protos \
+	--go_out ./protos --go_opt paths=source_relative \
+	--go-grpc_out ./protos --go-grpc_opt paths=source_relative \
+	--grpc-gateway_out ./protos --grpc-gateway_opt paths=source_relative \
+	protos/user/user.proto
 
 .PHONY: proto
--- main.go
+++ main.go
@@ -1,19 +1,76 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	userpb "github.com/skrevolve/grpc/protos/user"
 )
+type GRPCServer struct {
+	userpb.UnimplementedUserServiceServer
+}
+
+func NewGRPCServer() *GRPCServer {
+	return &GRPCServer{}
+}
+
+func (s *GRPCServer) Login(ctx context.Context, in *userpb.LoginRequest) (*userpb.LoginReply, error) {
+	return &userpb.LoginReply{AccessToken: in.Email + "_token"}, nil
+}
+
+func (s *GRPCServer) GetProfile(ctx context.Context, in *userpb.GetProfileRequset) (*userpb.GetProfileReply, error) {
+	return &userpb.GetProfileReply{Email: "sukyu0919@naver.com"}, nil
+}
 
 func main() {
-	var (
-		jsonAddr = flag.String("json", ":3000", "listen address of the json transport")
-		grpcAddr = flag.String("grpc", ":4000", "listen address of the grpc transport")
-		svc      = loggingService{&userService{}}
+	// listening ont TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("failed to listen:", err)
+	}
+
+	// create a gRPC server object
+	s := grpc.NewServer()
+	// attach the User service to the server
+	userpb.RegisterUserServiceServer(s, &GRPCServer{})
+	// serve gRPC Server
+	log.Println("serving gRPC on 0.0.0.0:8080")
+
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
-	flag.Parse()
+	if err != nil {
+		log.Fatalln("failed to dial server:", err)
+	}
 
-	go makeGRPCServerAndRun(*grpcAddr, svc)
+	gwmux := runtime.NewServeMux()
 
-	jsonServer := NewJSONAPIServer(*jsonAddr, svc)
-	jsonServer.Run()
+	// register UserService
+	err = userpb.RegisterUserServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr: ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
